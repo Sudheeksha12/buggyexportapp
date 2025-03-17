9770cbb13fd51be3dfeb7212f37c47be96c69af6
Sudheeksha12 <sudheeksha003@gmail.com>
2025-03-17 15:09:40 +0530

    Added simple table implementation without using libraries

--- src/App.js
+++ src/App.js
@@ -1,77 +1,57 @@
 import React from "react";
-import { Table, Button } from "antd";
-import * as XLSX from "xlsx";
-import { saveAs } from "file-saver";
 
-const dataSource = [
+const buggyData = [
   {
-    key: 1,
+    id: 1,
     vehicle: "CU-Buggy-6",
     route: "A to C",
     charges: 15,
     createdDate: "06-02-2024 15:50",
   },
   {
-    key: 2,
+    id: 2,
     vehicle: "CU-Buggy-1",
     route: "A to B",
     charges: 10,
     createdDate: "06-02-2024 15:50",
   },
- 
-  
-];
-
-const columns = [
   {
-    title: "Vehicle",
-    dataIndex: "vehicle",
-    key: "vehicle",
-  },
-  {
-    title: "From and To",
-    dataIndex: "route",
-    key: "route",
-  },
-  {
-    title: "Charges",
-    dataIndex: "charges",
-    key: "charges",
-  },
-  {
-    title: "Created Date",
-    dataIndex: "createdDate",
-    key: "createdDate",
+    id: 3,
+    vehicle: "CU-Buggy-1",
+    route: "A to C",
+    charges: 10,
+    createdDate: "06-02-2024 15:50",
   },
 ];
 
-  
-  const Export = () => {
-    var wsheet = XLSX.utils.json_to_sheet(dataSource  );
-   var wbook = XLSX.utils.book_new(); 
-   XLSX.utils.book_append_sheet(wbook, wsheet, "BuggyRoutes");
-
-   var excelBuffer = XLSX.write(wbook, { bookType: "xlsx", type: "array" });
-
-var b = new Blob([excelBuffer], {
-    type:"x"
-   });
-   saveAs(b,"buggy-routes.xlsx");
-  }
-  
-  const App = () => { 
+const TableComponent = () => {
   return (
-    <div style={{ padding: 20 }}>
-     <div style={{ marginBottom: 16, textAlign: "right" }}>
-        <Button type="primary" onClick={Export}>
-          Export to Excel
-        </Button>
-      </div>
-
-      <Table dataSource={dataSource} columns={columns} />;
-
+    <div style={{ padding: "20px" }}>
+      <h2>Buggy Data Table</h2>
+      <table style={{ width: "100%", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={thStyle}>Vehicle</th>
+            <th style={thStyle}>Route</th>
+            <th style={thStyle}>Charges</th>
+            <th style={thStyle}>Created Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {buggyData.map((item) => (
+            <tr key={item.id}>
+              <td style={tdStyle}>{item.vehicle}</td>
+              <td style={tdStyle}>{item.route}</td>
+              <td style={tdStyle}>{item.charges}</td>
+              <td style={tdStyle}>{item.createdDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default App;
+
+
+export default TableComponent;
